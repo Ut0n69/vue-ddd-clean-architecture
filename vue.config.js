444e25b6c730adf56c6c4ebe4a8143c2cd744ac4
yut0n <yt.sa.1117@gmail.com>
2019-05-22 20:38:28 +0900

    change: create client directory

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, '/src/client')
+      }
+    }
+  },
+  devServer: {
+    open: true,
+    port: 8080,
+    watchContentBase: true,
+    contentBase: 'src/client/',
+    inline: true,
+    hot: true,
+    progress: true
+  }
+}
