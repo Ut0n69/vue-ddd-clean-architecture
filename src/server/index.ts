c4abef23fbf8e88d0aa095b0292c0d002b2da352
yut0n <yt.sa.1117@gmail.com>
2019-05-25 21:38:23 +0900

    feat: member API

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,2 +1,32 @@
-const a: string = 'str'
-console.log(a)
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+const server = app.listen(port, () => {
+  console.log(`API server is listening on http://localhost:${port}`)
+})
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use((_: any, res: any, next: any) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
+let memberData = require('./memberData')
+
+app.get('/', (_: any, res: any) => {
+  res.json(memberData)
+})
+
+app.post('/', (req: any, res: any) => {
+  if (req.body.name && req.body.age && String(req.body.age).match(/^[0-9]+$/)) {
+    memberData.list.push(req.body)
+    res.json({ success: true })
+  } else {
+    res.json({ success: false })
+  }
+})
---
+++ src/server/memberData.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  list: [
+    {
+      name: 'Ross',
+      age: 30
+    },
+    {
+      name: 'Rachel',
+      age: 27
+    },
+    {
+      name: 'Monica',
+      age: 28
+    }
+  ]
+}
